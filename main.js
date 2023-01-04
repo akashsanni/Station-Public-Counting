9be3e7340bcb79d1ee74606ca2c57b66a1282649
Akash kumar <akash8825317696@gmail.com>
2023-01-04 11:34:38 +0530

    commit 1

---
+++ main.js
@@ -0,0 +1,35 @@
+// const countEl = document.getElementById("count-el")
+// let count =0
+// const saveEl = document.getElementById("save-el")
+// function increment(){
+//     count =count+1
+//     countEl.innerText= count
+
+// }
+
+// function save(){
+//     let countStr =" " +count + " - "
+    
+//     saveEl.innerText += countStr
+    
+//     count.style.color = 'red' //pending..
+
+// }
+
+
+const incrementBtn = document.querySelector('#increment-btn')
+const saveBtn = document.querySelector('#save-btn')
+const saveEl = document.querySelector('#save-el')
+let count = 0
+incrementBtn.addEventListener('click',function(){
+    count += 1
+    document.querySelector('#count-el').innerText = count
+    // console.log('clicked!')
+})
+
+saveBtn.addEventListener('click',function(){
+    // console.log(count)
+    saveEl.innerText += " " + count + " - "
+    count=0
+    document.querySelector('#count-el').innerText = count
+})
